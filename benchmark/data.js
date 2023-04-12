f4fe67cca71b851445fa7d0da8cee540a4a8c57e
github-action-benchmark <github@users.noreply.github.com>
2023-04-12 20:00:45 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for d788b10e424f9c19760277df79831c0e75c684ee

---
+++ benchmark/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1681329644775,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "d788b10e424f9c19760277df79831c0e75c684ee",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-04-12T17:22:44Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/d788b10e424f9c19760277df79831c0e75c684ee"
+        },
+        "date": 1681329644173,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Total step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 35.66254185092182,
+            "unit": "fps"
+          },
+          {
+            "name": "Action generation fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 72855.72346708355,
+            "unit": "fps"
+          },
+          {
+            "name": "Physics step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 211.98904752097184,
+            "unit": "fps"
+          },
+          {
+            "name": "Render step time (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 45.579440706684416,
+            "unit": "fps"
+          },
+          {
+            "name": "Non physics step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 754.0606476828788,
+            "unit": "fps"
+          }
+        ]
+      }
+    ]
+  }
+}
