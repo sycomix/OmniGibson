6a80b4b8642a6a61fcaa7c6d651743df208ccd0e
github-action-benchmark <github@users.noreply.github.com>
2023-04-17 01:03:45 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 66d11e84342912400c02c1318d8a890439dd6ca2

--- benchmark/data.js
+++ benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681329644775,
+  "lastUpdate": 1681693424990,
   "repoUrl": "https://github.com/StanfordVL/OmniGibson",
   "entries": {
     "Benchmark": [
@@ -44,6 +44,51 @@ window.BENCHMARK_DATA = {
           {
             "name": "Non physics step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
             "value": 754.0606476828788,
+            "unit": "fps"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "66d11e84342912400c02c1318d8a890439dd6ca2",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-04-14T06:16:01Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/66d11e84342912400c02c1318d8a890439dd6ca2"
+        },
+        "date": 1681693424400,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Total step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 35.78658136300987,
+            "unit": "fps"
+          },
+          {
+            "name": "Action generation fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 70951.60280808593,
+            "unit": "fps"
+          },
+          {
+            "name": "Physics step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 216.1070523130254,
+            "unit": "fps"
+          },
+          {
+            "name": "Render step time (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 45.57488182384542,
+            "unit": "fps"
+          },
+          {
+            "name": "Non physics step fps (Rs_int, without Fetch, without particles & softbody flatcache off)",
+            "value": 759.3438325436898,
             "unit": "fps"
           }
         ]
