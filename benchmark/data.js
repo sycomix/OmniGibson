62941a07ece8e11b7b76e29c3007aab1a79595f8
github-action-benchmark <github@users.noreply.github.com>
2023-04-19 22:07:14 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 72615e4e7a7b483185c390318bd3a03a9d2169a4

---
+++ benchmark/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1681942034089,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "72615e4e7a7b483185c390318bd3a03a9d2169a4",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-04-18T14:04:33Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/72615e4e7a7b483185c390318bd3a03a9d2169a4"
+        },
+        "date": 1681942032992,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Total step fps (Rs_int, without Fetch, without particles & cloth flatcache on)",
+            "value": 21.128323855493544,
+            "unit": "fps"
+          },
+          {
+            "name": "Action generation fps (Rs_int, without Fetch, without particles & cloth flatcache on)",
+            "value": 90930.13441248736,
+            "unit": "fps"
+          },
+          {
+            "name": "Physics step fps (Rs_int, without Fetch, without particles & cloth flatcache on)",
+            "value": 25.981507182430686,
+            "unit": "fps"
+          },
+          {
+            "name": "Render step time (Rs_int, without Fetch, without particles & cloth flatcache on)",
+            "value": 130.04580314877788,
+            "unit": "fps"
+          },
+          {
+            "name": "Non physics step fps (Rs_int, without Fetch, without particles & cloth flatcache on)",
+            "value": 906.5916584049986,
+            "unit": "fps"
+          }
+        ]
+      }
+    ]
+  }
+}
