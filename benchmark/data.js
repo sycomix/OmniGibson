54adfe09d5a6c116c85e4fc87e8abc4f01a70198
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 21:42:47 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 2ebcff21f75a8c3a96b59d3c67eaf17581fe4b59

---
+++ benchmark/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1682113367280,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "2ebcff21f75a8c3a96b59d3c67eaf17581fe4b59",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-04-21T07:05:24Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/2ebcff21f75a8c3a96b59d3c67eaf17581fe4b59"
+        },
+        "date": 1682113366114,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 10.079246113577565,
+            "unit": "fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 833.2938416088252,
+            "unit": "fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 12.039164115841164,
+            "unit": "fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 92.21889986912784,
+            "unit": "fps"
+          }
+        ]
+      }
+    ]
+  }
+}
