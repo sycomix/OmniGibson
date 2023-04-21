02c5466b63624f9fdec14ad5ab28bcdad5cebd71
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 22:15:15 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fd3650046ea7b502c7f243461a1f802e3f493119

---
+++ benchmark/data.js
@@ -0,0 +1,76 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1682115315505,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "fd3650046ea7b502c7f243461a1f802e3f493119",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-04-21T07:05:24Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/fd3650046ea7b502c7f243461a1f802e3f493119"
+        },
+        "date": 1682115313805,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Rs_int without Fetch, without particles & cloth, flatcache on)",
+            "value": 21.149384917194613,
+            "unit": "fps",
+            "extra": "Total frame fps"
+          },
+          {
+            "name": "Rs_int without Fetch, without particles & cloth, flatcache on)",
+            "value": 25.981984115301263,
+            "unit": "fps",
+            "extra": "Physics step fps"
+          },
+          {
+            "name": "Rs_int without Fetch, without particles & cloth, flatcache on)",
+            "value": 131.24063053934404,
+            "unit": "fps",
+            "extra": "Render step fps"
+          },
+          {
+            "name": "Rs_int without Fetch, without particles & cloth, flatcache on)",
+            "value": 888.6927656809498,
+            "unit": "fps",
+            "extra": "Non-physics step fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 10.127428130855174,
+            "unit": "fps",
+            "extra": "Total frame fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 12.11178779930068,
+            "unit": "fps",
+            "extra": "Physics step fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 92.09927204249698,
+            "unit": "fps",
+            "extra": "Render step fps"
+          },
+          {
+            "name": "Rs_int with Fetch, with particles & cloth, flatcache off)",
+            "value": 345.220529228446,
+            "unit": "fps",
+            "extra": "Non-physics step fps"
+          }
+        ]
+      }
+    ]
+  }
+}
