631643d83468dfcb00209f084da73934b4522f4a
github-action-benchmark <github@users.noreply.github.com>
2023-06-03 04:41:19 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f941cec80ec108d8a812df1dcb21d2533d83bb72

---
+++ benchmark/data.js
@@ -0,0 +1,64 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1685767279137,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "f941cec80ec108d8a812df1dcb21d2533d83bb72",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-05-23T12:27:06Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/f941cec80ec108d8a812df1dcb21d2533d83bb72"
+        },
+        "date": 1685767277487,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Rs_int with Fetch, without particles & cloth, flatcache on)",
+            "value": 16.676705495975753,
+            "unit": "fps",
+            "extra": [
+              "Total frame fps",
+              "Total frame fps"
+            ]
+          },
+          {
+            "name": "Rs_int with Fetch, without particles & cloth, flatcache on)",
+            "value": 22.23700183103542,
+            "unit": "fps",
+            "extra": [
+              "Physics step fps",
+              "Physics step fps"
+            ]
+          },
+          {
+            "name": "Rs_int with Fetch, without particles & cloth, flatcache on)",
+            "value": 105.53302897427352,
+            "unit": "fps",
+            "extra": [
+              "Render step fps",
+              "Render step fps"
+            ]
+          },
+          {
+            "name": "Rs_int with Fetch, without particles & cloth, flatcache on)",
+            "value": 343.14736759431366,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step fps",
+              "Non-physics step fps"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
