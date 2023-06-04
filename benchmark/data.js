38eceba9edcdd4cc2f8f1ffb697e18f49a17bc18
github-action-benchmark <github@users.noreply.github.com>
2023-06-04 01:44:30 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 34ce168429c0bf90643b53ea4d3d3eef7d67e98d

---
+++ benchmark/data.js
@@ -0,0 +1,388 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1685843070555,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "34ce168429c0bf90643b53ea4d3d3eef7d67e98d",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-06-03T12:45:14Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/34ce168429c0bf90643b53ea4d3d3eef7d67e98d"
+        },
+        "date": 1685843069348,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 18.521676858266193,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 13.496994972229004,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 4.89222526550293,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 0.0744938850402832,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, flatcache on",
+            "value": 36.70940160751343,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, flatcache on",
+            "value": 28.39665651321411,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, flatcache on",
+            "value": 7.380196253458659,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, flatcache on",
+            "value": 0.8838303883870443,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch",
+            "value": 71.63509527842203,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch",
+            "value": 55.91329177220662,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch",
+            "value": 10.514439741770428,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch",
+            "value": 2.6900180180867515,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Empty scene, with Fetch, flatcache on",
+            "value": 51.16232713063558,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Empty scene, with Fetch, flatcache on",
+            "value": 33.79086891810099,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Empty scene, with Fetch, flatcache on",
+            "value": 6.657440662384033,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Empty scene, with Fetch, flatcache on",
+            "value": 0.07406552632649739,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, flatcache on",
+            "value": 63.14395109812419,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, flatcache on",
+            "value": 47.0292592048645,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, flatcache on",
+            "value": 9.27687406539917,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, flatcache on",
+            "value": 2.8386473655700684,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Benevolence_0_int, with Fetch, flatcache on",
+            "value": 42.673726876576744,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Benevolence_0_int, with Fetch, flatcache on",
+            "value": 30.87561289469401,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Benevolence_0_int, with Fetch, flatcache on",
+            "value": 8.579486211140951,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Benevolence_0_int, with Fetch, flatcache on",
+            "value": 0.5342801411946615,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, fluids",
+            "value": 72.6101811726888,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, fluids",
+            "value": 56.650217374165855,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, fluids",
+            "value": 10.543045202891031,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, fluids",
+            "value": 2.670923868815104,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth",
+            "value": 91.88485542933147,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth",
+            "value": 76.49463415145874,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth",
+            "value": 10.149693489074707,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth",
+            "value": 2.6734439531962075,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, macro particles",
+            "value": 84.10261154174805,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, macro particles",
+            "value": 65.47005494435629,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, macro particles",
+            "value": 10.902191797892252,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, macro particles",
+            "value": 2.74609645207723,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth, fluids, macro particles",
+            "value": 111.27088069915771,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth, fluids, macro particles",
+            "value": 94.42320505777994,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth, fluids, macro particles",
+            "value": 11.405320167541504,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, with Fetch, cloth, fluids, macro particles",
+            "value": 2.7341882387797036,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
