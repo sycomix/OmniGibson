a195a0e478d77ecf85b1b08b2e04e2777f103288
github-action-benchmark <github@users.noreply.github.com>
2023-06-04 00:34:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 406e95a9e914726ef569b02f2e134bbe2845fa3a

---
+++ benchmark/data.js
@@ -0,0 +1,100 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1685838891468,
+  "repoUrl": "https://github.com/StanfordVL/OmniGibson",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "committer": {
+            "name": "StanfordVL",
+            "username": "StanfordVL"
+          },
+          "id": "406e95a9e914726ef569b02f2e134bbe2845fa3a",
+          "message": "[WIP] Profiling",
+          "timestamp": "2023-06-03T12:45:14Z",
+          "url": "https://github.com/StanfordVL/OmniGibson/pull/139/commits/406e95a9e914726ef569b02f2e134bbe2845fa3a"
+        },
+        "date": 1685838890038,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 18.38182846705119,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 13.472918669382732,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 4.776570796966553,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Empty scene, flatcache on",
+            "value": 0.07462422053019206,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, , flatcache on",
+            "value": 36.42273982365926,
+            "unit": "fps",
+            "extra": [
+              "Total frame time",
+              "Total frame time"
+            ]
+          },
+          {
+            "name": "Rs_int, , flatcache on",
+            "value": 28.201760450998943,
+            "unit": "fps",
+            "extra": [
+              "Physics step time",
+              "Physics step time"
+            ]
+          },
+          {
+            "name": "Rs_int, , flatcache on",
+            "value": 7.304636637369791,
+            "unit": "fps",
+            "extra": [
+              "Render step time",
+              "Render step time"
+            ]
+          },
+          {
+            "name": "Rs_int, , flatcache on",
+            "value": 0.8684134483337402,
+            "unit": "fps",
+            "extra": [
+              "Non-physics step time",
+              "Non-physics step time"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
